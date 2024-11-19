14a4e3c15a9a92dc06abe9505c551db0f0fe0eeb
Simoncastagne42 <simoncastagne42@gmail.com>
2024-11-19 11:37:27 +0100

    done : 2e exo

--- Exercice_2/script.js
+++ Exercice_2/script.js
@@ -1,9 +1,11 @@
-let mask= document.querySelector("#masquer");
+let mask = document.querySelector("#masquer");
+let print = document.querySelector("#afficher");
 let divtext = document.querySelector("#text");
-mask.addEventListener("click", handleMouseClick);
-function handleMouseClick () {
-    
-
-
-
+mask.addEventListener("click", handleMouseClickMasquer);
+function handleMouseClickMasquer() {
+  divtext.style.display = "none";
+}
+print.addEventListener("click", handleMouseClickAfficher);
+function handleMouseClickAfficher() {
+  divtext.style.display = "block";
 }
